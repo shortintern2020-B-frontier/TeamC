271f0e3c79a46f73f52392b15a9361ca4de3a350
村田翔太朗 <murata@MacBook-Pro.local>
2020-09-07 09:48:49 +0900

    add get and post function

--- app/pages/mypage.vue
+++ app/pages/mypage.vue
@@ -1,58 +1,61 @@
-
-<script>
-  export default {
-    async asyncData({ $axios }) {
-    const data = await $axios.$get('/users/', {params: "user_id"})
-    console.log(data[0])
-    return { data }
-      }
-  }
-
-</script>
-
 <template>
   <form>
-    <div id="profile">
+    <div id="app">
       <div id="icon">
         <v-avatar>
-          <img src="@/static/v.png">
+          <img src="@/static/v.png" />
         </v-avatar>
-          <v-btn>画像を変更する</v-btn>
+        <v-btn @click="setImg">画像を変更する</v-btn>
       </div>
-      <v-text-field
-        v-model="userId"
-        :counter="10"
-        label="ID"
-        placeholder="user_id"
-        required
-      ></v-text-field>
-      <v-text-field
-        v-model="name"
-        label="Name"
-        placeholder="太郎"
-        required
-      ></v-text-field>
-      <v-text-field
-        v-model="email"
-        label="E-mail"
-        placeholder="example@gmail.com"
-      ></v-text-field>
-
-      <v-text-field
-        v-model="birstday"
-        label="Birth Day"
-        placeholder="2020/1/1"
-      ></v-text-field>
-      <v-text-field
-        v-model="comment"
-        label="Comment"
-        placeholder="コメント"
-        
-      ></v-text-field>
+      <v-text-field v-model="user_id" :counter="10" label="ID" required></v-text-field>
+      <v-text-field v-model="username" label="Name" required></v-text-field>
+      <v-text-field v-model="email" label="E-mail" required></v-text-field>
+      <v-btn @click="postProfile">プロフィールを更新する</v-btn>
     </div>
     <div id="status">
       <h1>Status</h1>
-
+      <v-text-field v-model="comment" label="Comment"></v-text-field>
+      <div id="components-demo">
+        <Status></Status>
+      </div>
     </div>
   </form>
 </template>
+
+<script>
+import Vue from "vue";
+import Status from "~/components/Status";
+
+Vue.component("Status", Status);
+export default {
+  name: "mypage",
+  async asyncData({ $axios }) {
+    const data = await $axios.$get("/users/find", {
+      params: { user_id: "string" },
+    });
+    return data;
+  },
+  methods: {
+    setUserId: function () {},
+
+    createJson: function (key, value) {
+      var json;
+      return (json[key] = value);
+    },
+    postProfile: async function () {
+      console.log(this.id);
+      var json;
+      await this.$axios.$post("/users/update", {
+        user_id: this.user_id,
+        username: this.username,
+        email: this.email,
+        password: this.password,
+        status: 0,
+        comment: this.comment,
+      });
+    },
+  },
+  mounted: function () {},
+};
+</script>
+
