a65cb0d2e8d94ed3cbf256daf27dfde8516dddd6
Wolfsin <choose.bc.wolf@gmail.com>
2020-09-07 14:24:11 +0900

    create home page

---
+++ app/pages/home.vue
@@ -0,0 +1,22 @@
+// Author: ZHANG CHI
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="fill-height" fluid> </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  layout: "home",
+  data: () => ({
+    valid: true,
+    lazy: false,
+
+  }),
+  methods: {
+
+    }
+};
+</script>
