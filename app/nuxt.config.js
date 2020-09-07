704edcb7969d0978ac10f6583c2189b2801d2d25
Wolfsin <choose.bc.wolf@gmail.com>
2020-09-07 10:36:32 +0900

    add axios and js-sha256

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -23,6 +23,11 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
+    script: [
+      {
+        src: 'https://unpkg.com/axios/dist/axios.min.js'
+      }
+    ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
