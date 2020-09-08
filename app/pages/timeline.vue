ab1f5da987530f16d3eec323cd0d0a06f26ae94f
Hiroki Okubo <hokubo2126@gmail.com>
2020-09-08 14:26:16 +0900

    modify timeline.vue

--- app/pages/timeline.vue
+++ app/pages/timeline.vue
@@ -1,67 +1,80 @@
-<!-- Author Hiroki Okubo -->
-
 <template>
-  <div class="container">
-   <table border="1">
-     <tbody>
-     <tr>
-       <th>ID</th>
-       <th>タイムライン</th>
-     </tr>
-     <tr v-for="item in items" :key="item.id">
-       <td>{{ item.id }}</td>
-       <td>{{ item.name }}</td>
-     </tr>
-     </tbody>
-   </table>
- </div>
+  <v-app>
+    <v-main>
+      <!-- <v-app-bar color="" dense></v-app-bar> -->
+      <v-container class="fill-height" fluid>
+        <v-container>
+          <v-row dense>
+
+            <v-card>タイムライン</v-card>
+            <v-spacer></v-spacer>
+            <v-col v-for="(timeline) in timelines_list" :key="timeline.id" cols="12">
+              <v-card>
+                <div class="d-flex flex-no-wrap justify-space-between">
+                  <v-avatar size="100" tile>
+                    <span class="white--text headline">{{
+                      timeline.name
+                    }}</span>
+                  </v-avatar>
+                  <div>
+                    <v-card-title
+                      class="headline"
+                      v-text="timeline.statics"
+                    ></v-card-title>
+                    <v-card-subtitle>
+                      {{ timeline.comment }} + {{ timeline.date }} + {{ timeline.place }}
+                    </v-card-subtitle>
+                  </div>
+                </div>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
-
-
-
-<!-- 遷移ページ -->
-<!-- export default {
-  created(){
-  this.chats = await this.$axios.get(`/chats/${this.id}`);
-}
-} -->
 
 <script>
 export default {
-  data() {
-    return {
-      items: []
+  layout: "home",
+  data: () => ({
+    // timelines_list: [
+    //   {
+    //     id: 0,
+    //     name: "jonh",
+    //     date: "2020/9/9",
+    //     place: "Shinjyuku",
+    //     comment: "だれか遊びに行きませんか？"
+    //   },
+    //   {
+    //     id: 1,
+    //     name: "jonh",
+    //     date: "2020/9/9",
+    //     place: "Shinjyuku",
+    //     comment: "だれか遊びに行きませんか？"
+    //   }
+    // ],
+    timelines_list: []
+  }),
+  methods: {
+    findData: async function() {
+      let userid = this.$store.state.user.userInfo.id;
+      const res = await this.$axios.$get(
+        "http://127.0.0.1:8000/timelines",
+        {
+          params: { id: userid }
+        }
+      );
+      if (res != null) {
+        this.timelines_list = res;
+        console.log(this.timelines_list);
+      }
     }
   },
-  mounted: function() {
-    this.$axios
-      // .$get('/timelines')
-       // http://127.0.0.1:8000/users/login
-      .$post('/users/login')
-      .then(response => {
-        this.items = response
-      })
-      .catch(error => {
-        console.log(error)
-      })
+  created() {
+    this.findData();
   }
-}
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-</style>
-
-<!-- from Hirata-san
-database からデータを取る
-
-async created() {
-  this.timelines = await this.$axios.get(`/timelines?user_id=${this.user_id}`);
-} -->
+© 2020 GitHub, Inc.
