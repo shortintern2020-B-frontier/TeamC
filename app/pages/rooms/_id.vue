3f26512d83706c5e5e9d438c6527f92806d86c6d
r0ar-69 <shun199969kiyo@gmail.com>
2020-09-09 11:32:59 +0900

    update room
    リアルタイムチャットできた！

--- app/pages/rooms/_id.vue
+++ app/pages/rooms/_id.vue
@@ -23,34 +23,56 @@
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
-import VuetifyLogo from "~/components/VuetifyLogo.vue";
-
+import moment from 'moment/moment'
 export default {
-  components: {
-    Logo,
-    VuetifyLogo,
-  },
   asyncData({ params }) {
     const { id } = params;
     return { chat_room_id: id };
   },
+  
   async created() {
     const res = await this.$axios.get("/chats", {
       params: {
-        chat_room_id: this.chat_room_id,
+        chat_room_id: 1,//this.chat_room_id,
       },
     }); 
+    const _this = this;
     this.messages = res.data;
+
+    var ws = new WebSocket(`ws://localhost:8000/ws/1`);
+    ws.onmessage = function(event) {
+
+                /*var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)*/
+
+                //リスト更新の処理を書く
+                console.log("seeeeeeeentt!!!");
+                _this.messages.push(JSON.parse(event.data));
+    };
+    
   },
     methods: {
-    sendMessage: async function () {
+    sendMessage: async function (event) {
+      var ws = new WebSocket(`ws://localhost:8000/ws/1`);
       console.log(this.send_message);
       await this.$axios.post("/chats/", {
         user_id: 1,//this.$store.state.user.userInfo.id,
-        chat_room_id: this.chat_room_id,
+        chat_room_id: 1,//this.chat_room_id,
         content: this.send_message
       });
+      var chat_data = {
+        "id": null,
+        "user_id": 1,//this.userInfo.id,
+        "created_at": moment().format("YYYY/MM/DD HH:MM"),
+        "content": this.send_message,
+        "username": "shun"//this.userInfo.username
+      }
+      ws.send(JSON.stringify(chat_data))
+      this.send_message = ""
+      event.preventDefault()
     },
   },
   data: () => ({
@@ -60,10 +82,7 @@ export default {
       { header: "Chat" },
       {
         avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
-        title: "Brunch this weekend?",
-        subtitle:
-          "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
-      },
+        },
       { divider: true, inset: true }
     ],
   }),
