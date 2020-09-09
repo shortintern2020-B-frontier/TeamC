0520166728e2bc12f2bbafd925ddb9396ca35710
村田翔太朗 <murata@MacBook-Pro.local>
2020-09-09 09:35:21 +0900

    get timeline

--- app/pages/timeline.vue
+++ app/pages/timeline.vue
@@ -2,29 +2,27 @@
 <template>
   <v-app>
     <v-main>
-      <!-- <v-app-bar color="" dense></v-app-bar> -->
+      <v-btn to="/postTimeline">
+        <span>タイムラインを投稿</span>
+      </v-btn>
       <v-container class="fill-height" fluid>
-        <v-container>
+        <v-container v-if="is_friend">
           <v-row dense>
-
             <v-card>タイムライン</v-card>
             <v-spacer></v-spacer>
             <v-col v-for="(timeline) in timelines_list" :key="timeline.id" cols="12">
               <v-card>
                 <div class="d-flex flex-no-wrap justify-space-between">
                   <v-avatar size="100" tile>
-                    <span class="white--text headline">{{
-                      timeline.name
-                    }}</span>
+                    <span class="white--text headline">
+                      {{
+                      getFriendName(timeline.user_id)
+                      }}
+                    </span>
                   </v-avatar>
                   <div>
-                    <v-card-title
-                      class="headline"
-                      v-text="timeline.statics"
-                    ></v-card-title>
-                    <v-card-subtitle>
-                      {{ timeline.comment }} + {{ timeline.date }} + {{ timeline.place }}
-                    </v-card-subtitle>
+                    <v-card-title class="headline" v-text="timeline.statics"></v-card-title>
+                    <v-card-subtitle>{{ timeline.content }} + {{ processDate(timeline.event_date) }} + {{ timeline.place }}</v-card-subtitle>
                   </div>
                 </div>
               </v-card>
@@ -40,42 +38,37 @@
 export default {
   layout: "home",
   data: () => ({
-    // timelines_list: [
-    //   {
-    //     id: 0,
-    //     name: "jonh",
-    //     date: "2020/9/9",
-    //     place: "Shinjyuku",
-    //     comment: "だれか遊びに行きませんか？"
-    //   },
-    //   {
-    //     id: 1,
-    //     name: "jonh",
-    //     date: "2020/9/9",
-    //     place: "Shinjyuku",
-    //     comment: "だれか遊びに行きませんか？"
-    //   }
-    // ],
-    timelines_list: []
+    is_friend: false,
+    friend_list: [],
+    timelines_list: [],
   }),
   methods: {
-    findData: async function() {
+    findData: async function () {
       let userid = this.$store.state.user.userInfo.id;
-      const res = await this.$axios.$get(
-        "http://127.0.0.1:8000/timelines",
-        {
-          params: { id: userid }
-        }
-      );
+      const res = await this.$axios.$get("/timelines", {
+        params: { user_id: userid },
+      });
       if (res != null) {
         this.timelines_list = res;
-        console.log(this.timelines_list);
       }
-    }
+      const friends = await this.$axios.$get("/users/friends", {
+        params: { id: userid },
+      });
+      if (friends != null) {
+        this.is_friend = true;
+        this.friend_list = friends;
+      }
+    },
+    // Author Shotaro Murata
+    processDate: function (date) {
+      return new Date(date);
+    },
+    getFriendName: function (friend_id) {
+      return this.friend_list.filter((n) => n.id == friend_id)[0].username;
+    },
   },
   created() {
     this.findData();
-  }
+  },
 };
 </script>
-© 2020 GitHub, Inc.
